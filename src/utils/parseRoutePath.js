3da47e3184dc5b537ec784a69a9fc5178f56a264
Itamar Junior <cdajuniorf@gmail.com>
2025-02-24 14:40:52 -0300

    feat: Criei a rota para atualizar ticket.

--- src/utils/parseRoutePath.js
+++ src/utils/parseRoutePath.js
@@ -1,7 +1,7 @@
 export function parseRoutePath(path) {
-  const routeParametersRegex = /:([a-zA-Z]+)/g;
+  const routeParametersRegex = /:([a-zA-Z0-9_-]+)/g;
 
-  const params = path.replaceAll(routeParametersRegex, "(?<$1>[a-z0-9_]+)");
+  const params = path.replaceAll(routeParametersRegex, "(?<$1>[a-z0-9_-]{36})");
 
   const pathRegex = new RegExp(`^${params}(?<query>\\?(.*))?$`);
 
